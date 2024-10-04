bf664192509352443fb722cf78b702b6d8eed748
AAnishRam <123292130+AAnishRam@users.noreply.github.com>
2024-10-04 12:10:50 +0530

    Time: 10 ms (10.31%) | Memory: 44.4 MB (68.74%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -14,23 +14,19 @@ public class Solution {
 
         if(head==null)
             return head;
-            
-        boolean flag = true;
+
         ListNode temp = head;
         HashMap<ListNode,Integer> hm = new HashMap<>();
 
-        while(flag && temp.next!=null)
+        while(temp.next!=null)
         {
-            if(temp.next == null)
-                flag = false;
 
             if(hm.get(temp)!=null)
             {
-                flag = false;
                 return temp;
             }
         
-            hm.put(temp,hm.getOrDefault(temp,0)+1);
+            hm.put(temp,0);
             temp = temp.next;
 
         }
