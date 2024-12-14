31efa2f583e9c068fcbbfa1e276b155096a5667c
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2024-12-14 06:28:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (10.24%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if (n == 0) {
-            // No elements to merge from nums2
+        
+        if(n==0)
+            return;
+        
+        if(m==0)
+        {
+            nums1 = nums2;
             return;
         }
 
-        if (m == 0) {
-            // If nums1 has no valid elements, copy all elements from nums2
-            for (int i = 0; i < n; i++) {
-                nums1[i] = nums2[i];
+
+        int a = m-1;
+        int b = n-1;
+        int c = m+n-1;
+
+        while(a>=0 && b>=0)
+        {
+            if(nums1[a]<=nums2[b])
+            {
+                nums1[c--] = nums2[b--];
             }
-            return;
-        }
-
-        // Start merging from the back
-        int i = m - 1; // Last valid element in nums1
-        int j = n - 1; // Last element in nums2
-        int k = m + n - 1; // Last position in nums1
-
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
+            else
+            {
+                nums1[c--] = nums1[a--];
             }
         }
 
-        // If any elements remain in nums2, copy them to nums1
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
+         while (b >= 0) {
+            nums1[c--] = nums2[b--];
         }
+
+
     }
 };
