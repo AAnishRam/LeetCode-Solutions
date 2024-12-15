6abaed75693e786b8c6d76d0e411d44e657007c1
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2024-12-15 10:38:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.27%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    int f(int n, vector<int>& dp) {
-        if (n <= 1) return 1; // Base case, no need to update dp[n] here.
+
+    int f(int n, vector<int>& dp)
+    {
+        if(n<=1) return 1;
         if (dp[n] != -1) return dp[n]; // Use memoized value if available.
 
-        return dp[n] = f(n - 1, dp) + f(n - 2, dp); // Compute and store.
+
+        return dp[n] = f(n-1,dp) + f(n-2,dp);
     }
 
     int climbStairs(int n) {
-        vector<int> dp(n + 1, -1); // Initialize dp array.
-        return f(n, dp); // Call helper function.
+        vector<int> dp(n+1,-1);
+
+        return f(n,dp);
     }
 };
