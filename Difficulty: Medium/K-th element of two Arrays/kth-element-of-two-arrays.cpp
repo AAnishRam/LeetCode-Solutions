8cde54f5762cd8921eb572c8323d8f21d20410d6
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2024-12-16 21:51:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -6,55 +6,13 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int kthElement(int k, vector<int>& arr1, vector<int>& arr2) {
+    int kthElement(vector<int>& arr, vector<int>& brr, int k) {
         // code here
-        int m=0 ,n=0;
-        int current=0;
-        for (int i=0;i<k;i++)
-            {   if(arr1.size()>n && arr2.size()>m)
-                {
-                    if(arr2[m]<arr1[n])
-                        {   if(current<=arr2[m])
-                            {
-                                current=arr2[m];
-                                m++;
-                            }
-                            else{
-                            current=arr1[n];
-                            m++;}
-                        }
-                            
-                    else if(arr2[m]>arr1[n])
-                        {   if(current<=arr1[n])
-                                {
-                                    current=arr1[n];
-                                    n++;
-                                }
-                                else{
-                            current=arr2[m];
-                            n++;}
-                        }
-                        
-                    else
-                    {   current=arr1[n];
-                        m++;
-                        n++;
-                        i++;
-                    }
-                }
-                else if(arr1.size()>n && arr2.size()==m)
-                {
-                    current=arr1[n];
-                    n++;
-                }
-                else
-                {
-                    current=arr2[m];
-                    m++;
-                }
-               
-            }
-            return current;
+        vector<int> temp(arr.begin(), arr.end());
+        temp.insert(temp.end(), brr.begin(), brr.end());
+        sort(temp.begin(), temp.end());
+        return temp[k-1];
+        
     }
 };
 
@@ -71,22 +29,22 @@ int main() {
         cin.ignore();
         string input;
         int num;
-        vector<int> arr1, arr2;
+        vector<int> a, b;
 
         getline(cin, input);
         stringstream s2(input);
         while (s2 >> num) {
-            arr1.push_back(num);
+            a.push_back(num);
         }
 
         getline(cin, input);
         stringstream s3(input);
         while (s3 >> num) {
-            arr2.push_back(num);
+            b.push_back(num);
         }
 
         Solution ob;
-        cout << ob.kthElement(k, arr1, arr2) << endl;
+        cout << ob.kthElement(a, b, k) << endl << "~\n";
     }
     return 0;
 }
