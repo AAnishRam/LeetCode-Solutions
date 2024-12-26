faa8eb28ebd1060a0cba9c9d66ed5274b660a538
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2024-12-26 07:56:52 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (32.31%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int i = 1;
-        int n = nums.size();
-        int temp = nums[0];
+        // int i = 1;
+        // int n = nums.size();
+        // int temp = nums[0];
 
-        while(i<n && nums[i-1]<=nums[i])
-        {
-            i++;
-        }
+        // while(i<n && nums[i-1]<=nums[i])
+        // {
+        //     i++;
+        // }
 
-        if(i<n && nums[i]>temp ) return false;
+        // if(i<n && nums[i]>temp ) return false;
 
-        i++;
+        // i++;
 
-        while(i<n && nums[i-1]<= nums[i])
-        {
-            if(nums[i]>temp)
-                return false;
+        // while(i<n && nums[i-1]<= nums[i])
+        // {
+        //     if(nums[i]>temp)
+        //         return false;
             
-            i++;
+        //     i++;
+        // }
+
+        // if(i<nums.size())
+        //     return false;
+
+        // return true;
+
+        int count = 0;
+
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i-1]>nums[i])
+                count++;
         }
+        if(nums[nums.size()-1]>nums[0])count++;
 
-        if(i<nums.size())
-            return false;
-
-        return true;
+        return count<=1;
 
     }
 };
