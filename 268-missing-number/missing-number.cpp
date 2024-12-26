2b1534d6721544dc8e236f378d2408045a5d0765
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2024-12-26 21:08:26 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (38.28%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,27 +2,38 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         
-        int max = nums[0];
-        int sum = 0;
-        bool isZero = false;
+        // int max = nums[0];
+        // int sum = 0;
+        // bool isZero = false;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     sum+=nums[i];
+        //     if(max<nums[i])
+        //         max = nums[i];
+
+        //     if(nums[i]==0)
+        //         isZero = true;
+        // }
+
+        // if(!isZero)
+        //     return 0;
+
+        // int ans = (max*(max+1))/2;
+
+        // if(ans == sum)
+        //     return max+1;
+
+        // return ans-sum;
+
+
+        int xor1 = 0;
+        int xor2 = 0;
+
         for(int i=0;i<nums.size();i++)
         {
-            sum+=nums[i];
-            if(max<nums[i])
-                max = nums[i];
-
-            if(nums[i]==0)
-                isZero = true;
+            xor1^=(i+1);
+            xor2^=nums[i];
         }
-
-        if(!isZero)
-            return 0;
-
-        int ans = (max*(max+1))/2;
-
-        if(ans == sum)
-            return max+1;
-
-        return ans-sum;
+        return xor1^xor2;
     }
 };
