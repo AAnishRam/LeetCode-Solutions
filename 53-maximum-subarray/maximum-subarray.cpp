d10fe91dccfe50a30102a4a1ba3e2ae4ac9d1fb6
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2024-12-28 19:15:57 +0530

    Time: 3 ms (20.73%) | Memory: 71.8 MB (30.39%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+
+        //kadanes algorithm
+        // ansStart and ansEnd are the indexes of the subarray formed
+
         long long maxi = INT_MIN;
         long long sum = 0;
+
+        int start = 0;
+        int ansStart = -1;
+        int ansEnd = -1;
 
 
         for(int i=0;i<nums.size();i++)
         {
+            if(sum==0) start = i;
             sum+=nums[i];
-            maxi = max(maxi,sum);
+
+            if(sum>maxi)
+            {
+                maxi = sum;
+                ansStart = start;
+                ansEnd = i;
+            }
 
             if(sum<0)
             {
                 sum = 0;
+                
             }
         }
+
+        cout<<ansStart<<" "<<ansEnd;
         return maxi;
     }
 };
