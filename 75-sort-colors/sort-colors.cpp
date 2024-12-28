7b2634e0d65f6b0485d68643f801556de94552d3
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2024-12-28 13:52:01 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (16.75%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,30 +1,56 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        int i = 0;
-        int j = n-1;
-        int k = 0;
+        // my code
+        // int n = nums.size();
+        // int i = 0;
+        // int j = n-1;
+        // int k = 0;
 
-        while(k<n)
-        {
-            while(i<=k && nums[k]==0)
-            {
-                swap(nums[i],nums[k]);
-                i++;
-            }
-            while(k<=j && nums[k]==2)
-            {
-                swap(nums[j],nums[k]);
-                j--;
-            }
-            while(i<=k && nums[k]==0)
-            {
-                swap(nums[i],nums[k]);
-                i++;
-            }
-            k++;
+        // while(k<n)
+        // {
+        //     while(i<=k && nums[k]==0)
+        //     {
+        //         swap(nums[i],nums[k]);
+        //         i++;
+        //     }
+        //     while(k<=j && nums[k]==2)
+        //     {
+        //         swap(nums[j],nums[k]);
+        //         j--;
+        //     }
+        //     while(i<=k && nums[k]==0)
+        //     {
+        //         swap(nums[i],nums[k]);
+        //         i++;
+        //     }
+        //     k++;
            
+        // }
+
+        //Dutch national flag algorithm
+        int n = nums.size();
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[low],nums[mid]);
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[high],nums[mid]);
+                high--;
+            }
         }
     }
 };
