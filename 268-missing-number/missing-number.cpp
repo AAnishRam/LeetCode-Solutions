a485b7481ade3b3892b64837ccc4ed2cd119d867
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2024-12-30 14:17:49 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (24.88%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,49 +2,26 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         
-        // int max = nums[0];
+        //using sum of n natural numbers
+        // int n = nums.size();
+
         // int sum = 0;
-        // bool isZero = false;
-        // for(int i=0;i<nums.size();i++)
+        // for(int i=0;i<n;i++)
         // {
         //     sum+=nums[i];
-        //     if(max<nums[i])
-        //         max = nums[i];
-
-        //     if(nums[i]==0)
-        //         isZero = true;
         // }
+        // int temp = (n*(n+1))/2;
 
-        // if(!isZero)
-        //     return 0;
+        // return temp-sum;
+        int xor1 = 0;
+        int xor2 = 0;
 
-        // int ans = (max*(max+1))/2;
-
-        // if(ans == sum)
-        //     return max+1;
-
-        // return ans-sum;
-
-
-        // int xor1 = 0;
-        // int xor2 = 0;
-
-        // for(int i=0;i<nums.size();i++)
-        // {
-        //     xor1^=(i+1);
-        //     xor2^=nums[i];
-        // }
-        // return xor1^xor2;
-
-        int n = nums.size();
-
-        int sum = 0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<nums.size();i++)
         {
-            sum+=nums[i];
+            xor1^=(i+1);
+            xor2^=nums[i];
         }
-        int temp = (n*(n+1))/2;
+        return xor1^xor2;
 
-        return temp-sum;
     }
 };
