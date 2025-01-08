b34643c844300ed0b2053d70fda0e7396474928b
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2025-01-08 11:37:49 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (29.53%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) 
+            return 0;
+
+        int jumps = 0;
+        int currentReach = 0;
+        int maxReach = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            maxReach = max(maxReach, i + nums[i]);
+            if (i == currentReach) {
+                jumps++;
+                currentReach = maxReach;
+                if (currentReach >= n - 1)
+                    break;
+            }
+        }
+
+        return jumps;
+    }
+};
