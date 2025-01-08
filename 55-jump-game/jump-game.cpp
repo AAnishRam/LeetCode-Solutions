530c9af180b65157dff2fbbb06f98e1144f6bed5
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2025-01-08 10:25:58 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (47.51%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int i = 0;
+        // int i = 0;
 
         int n = nums.size();
 
-        while(i<n-1)
-        {
-            if(nums[i]==0)
-                return false;
+        // while(i<n-1)
+        // {
+        //     if(nums[i]==0)
+        //         return false;
 
-            int maxi = -1;
-            int index = i;
-            for(int j = 1; j<=nums[i] && i+j<n ; j++)
-            {
-                 if (maxi < i + j + nums[i + j]) { 
-                    maxi = i + j + nums[i + j];
-                    index = i + j;
-                }
-            }
+        //     int maxi = -1;
+        //     int index = i;
+        //     for(int j = 1; j<=nums[i] && i+j<n ; j++)
+        //     {
+        //          if (maxi < i + j + nums[i + j]) { 
+        //             maxi = i + j + nums[i + j];
+        //             index = i + j;
+        //         }
+        //     }
 
-            i = index;
-        }
+        //     i = index;
+        // }
        
 
-        return true;
+        // return true;
+
+        int canReach = nums[0];
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(canReach>=n-1)
+                return true;
+
+            if(canReach < i+nums[i])
+                canReach = i+nums[i];
+
+            if(i==canReach)
+                return false;
+        }
+
+        return false;
     }
 };
