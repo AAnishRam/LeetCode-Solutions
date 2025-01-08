efe58dc75abcec11ad0aa53900525385c548604a
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2025-01-08 12:35:47 +0530

    Time: 1 ms (48.81%) | Memory: 8.7 MB (77.42%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
-    int getLucky(string s, int k) {
-        // Convert each character in the string to its corresponding numeric value
-        string number = "";
-        for (char x : s) {
-            number += to_string(x - 'a' + 1);
-        }
-        
-        // Perform the transformation `k` times
-        while (k > 0) {
-            int temp = 0;
-            for (char x : number) {
-                temp += x - '0';  // Sum the digits of the current number
+
+    long long countWord(string str)
+    {
+        long long ans = 0;
+
+        for(int i=0;i<str.size();i++)
+        {
+            int temp = str[i]-'a'+1LL;
+            
+            while(temp!=0)
+            {
+                ans += temp%10;
+                temp/=10;
             }
-            number = to_string(temp);  // Convert the sum back to a string
-            k--;
         }
-        return stoi(number);  // Return the final result as an integer
+        return ans;
+    }
+
+    long long transform(long long n)
+    {
+        long long sum = 0;
+        while(n!=0) 
+        {
+            sum+=(n%10);
+            n/=10;
+        }
+        return sum;
+    }
+
+    int getLucky(string s, int k) {
+        
+            long long n = countWord(s);
+
+            cout<<n;
+
+            for(int i = 2;i<=k;i++)
+               n = transform(n);
+
+            return n;
+
+
     }
 };
