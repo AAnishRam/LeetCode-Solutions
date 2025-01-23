bfbdcb9ee270ccae29be74a5261162a08a2502a2
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2025-01-23 12:13:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (56.67%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        
+        int i = 0;
+        int ans = 0;
+        int sign = 1;
+
+        while(i<s.length() && s[i] == ' ')
+            i++;
+        
+
+        if(i < s.length() && (s[i]=='-' || s[i] =='+'))
+        {
+            if(s[i] == '-')
+                sign *= -1;
+            else
+                sign *= +1;
+
+            i++;
+        }
+
+        while(i<s.length() && s[i] == '0')
+            i++;
+        
+        while(i<s.length() && s[i] >= '0' && s[i]<='9')
+        {
+            int digit = s[i] - '0';
+
+            if (ans > (INT_MAX - digit) / 10) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+
+            ans = ans * 10 + digit;
+            i++;
+        }
+
+        return ans*sign;
+    }
+};
