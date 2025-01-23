113607ae6c11555f8ea62d7fcb33ab8d81525c0b
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2025-01-23 06:34:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (79.28%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -3,36 +3,62 @@ public:
     string removeOuterParentheses(string s) {
         int n = s.length();
 
-        string ans = "";
-        stack<char>st;
+        // my approach
+        // string ans = "";
+        // stack<char>st;
 
-        string temp = "";
+        // string temp = "";
+
+        // for(int i = 0; i < n; i++)
+        // {
+        //     if(s[i] == '(')
+        //     {
+        //         st.push(s[i]);
+        //         temp+=s[i];
+        //     }
+        //     else if(s[i] == ')')
+        //     {
+        //         if(st.top() == '(')
+        //         {
+        //             temp+=s[i];
+        //             st.pop();
+        //         }
+        //         else
+        //         {
+        //             st.push(s[i]);
+        //             temp+=s[i];
+        //         }
+        //     }
+
+        //     if(st.empty())
+        //     {
+        //         ans += temp.substr(1,temp.length()-2);
+        //         temp = "";
+        //     }
+        // }
+
+        // return ans;
+
+
+        //optimal one
+        string ans = "";
+        int count = 0;
 
         for(int i = 0; i < n; i++)
         {
             if(s[i] == '(')
             {
-                st.push(s[i]);
-                temp+=s[i];
-            }
-            else if(s[i] == ')')
-            {
-                if(st.top() == '(')
-                {
-                    temp+=s[i];
-                    st.pop();
-                }
-                else
-                {
-                    st.push(s[i]);
-                    temp+=s[i];
-                }
-            }
+                if(count > 0)
+                    ans += s[i];
 
-            if(st.empty())
+                count++;
+            }
+            else
             {
-                ans += temp.substr(1,temp.length()-2);
-                temp = "";
+                if(count > 1)
+                    ans += s[i];
+                    
+                count--;
             }
         }
 
