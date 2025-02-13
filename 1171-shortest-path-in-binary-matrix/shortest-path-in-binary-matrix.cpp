790242160d4267d5009a90107ac5a6ce7f084019
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2025-02-13 21:45:37 +0530

    Time: 16 ms (70.84%) | Memory: 26.3 MB (44.98%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -3,10 +3,13 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         queue<pair<pair<int, int>, int>>q;
-        vector<vector<int>> visited(n, vector<int>(n, 0));
+        // vector<vector<int>> visited(n, vector<int>(n, 0));
+        vector<vector<int>> distance(n, vector<int>(grid[0].size(), INT_MAX));
 
         q.push({{0, 0}, 0});
-        visited[0][0] = 1;
+        // visited[0][0] = 1;
+        distance[0][0] = 0;
+
 
         if(grid[0][0] == 1)
             return -1;
@@ -25,10 +28,7 @@ public:
             q.pop();
             
             if(r == n-1 && c == n-1)
-            {
-                ans = min(ans, dist);
-                break;
-            }
+                return distance[r][c] + 1;
 
             for(int i=0;i<8;i++)
             {
@@ -37,19 +37,20 @@ public:
                 int newDist = dist + 1;
 
                 if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < n && 
-                grid[newRow][newCol] == 0 && !visited[newRow][newCol])
+                grid[newRow][newCol] == 0 && dist + 1 < distance[newRow][newCol]
+                // !visited[newRow][newCol]
+                )
                 {
-                    visited[newRow][newCol] = 1;
+                    distance[newRow][newCol] = newDist;
                     q.push({{newRow, newCol}, newDist});
+
                 }
             }
 
 
         }
 
-        if(ans == INT_MAX)
-            return -1;
-
-        return ans + 1;
+        
+        return -1;
     }
 };
