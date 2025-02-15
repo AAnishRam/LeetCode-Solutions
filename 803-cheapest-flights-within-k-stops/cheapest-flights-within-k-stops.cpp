1f80c7a35155ea6f3f03252ee5d71a2bc4b1526c
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2025-02-15 07:23:36 +0530

    Time: 7 ms (27.94%) | Memory: 18.2 MB (42.89%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,35 +1,50 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
         vector<vector<pair<int, int>>> adj(n);
-        for (auto& it : flights) {
-            adj[it[0]].push_back({it[1], it[2]});
+        for(auto it : flights)
+        {
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            adj[u].push_back({v, wt});
         }
 
-        vector<int> prices(n, INT_MAX); // Stores the best price for each city
-        queue<pair<int, pair<int, int>>> pq; // {stops, {cost, city}}
+        vector<int>prices(n, INT_MAX);
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
+        greater<pair<int, pair<int, int>>>> pq;
+        // {steps, {cost, flightNumber}}
         pq.push({0, {0, src}});
         prices[src] = 0;
 
-        while (!pq.empty()) {
-            auto [stops, node] = pq.front();
+        while(!pq.empty())
+        {
+            auto iterator = pq.top();
             pq.pop();
-            int price = node.first;
-            int city = node.second;
+            int step = iterator.first;
+            int price = iterator.second.first;
+            int flight = iterator.second.second;
 
-            if (stops > k) continue; // We can take at most k stops
+            if (step > k) continue; 
 
-            for (auto& [nextCity, flightPrice] : adj[city]) {
-                int newPrice = price + flightPrice;
 
-                // We push into the queue if it's a better price for the given stop count
-                if (newPrice < prices[nextCity]) {
-                    prices[nextCity] = newPrice;
-                    pq.push({stops + 1, {newPrice, nextCity}});
+            for(auto it : adj[flight])
+            {
+                int adjFlight = it.first;
+                int adjPrice = it.second;
+
+                int newPrice = price + adjPrice;
+
+                if(newPrice < prices[adjFlight])
+                {
+                    prices[adjFlight] = newPrice;
+                    pq.push({step+1, {newPrice, adjFlight}});
                 }
             }
         }
 
         return prices[dst] == INT_MAX ? -1 : prices[dst];
+
     }
 };
