6cbeb0876c3f71e11c61bd9b97c72a1e573ddbe0
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2025-02-17 21:23:00 +0530

    Time: 8 ms (24.26%) | Memory: 15.8 MB (25.44%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    void frameAdj(map<int, vector<int>> &hm, TreeNode*root)
+    {
+        if(root == nullptr)
+            return;
+        
+        if(root->left != nullptr)
+        {
+            hm[root->val].push_back(root->left->val);
+            hm[root->left->val].push_back(root->val);
+            frameAdj(hm, root->left);
+        }
+        
+        if(root->right != nullptr)
+        {
+            hm[root->val].push_back(root->right->val);
+            hm[root->right->val].push_back(root->val);
+            frameAdj(hm, root->right);
+        }
+        
+    }
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        map<int, vector<int>>hm;
+        frameAdj(hm, root);
+
+
+        vector<int> ans;
+        queue<pair<int, int>>q;
+        map<int, int> visited;
+
+        q.push({target->val, 0});
+        visited[target->val] = 1;
+
+        while(!q.empty())
+        {
+            int node = q.front().first;
+            int step = q.front().second;
+            q.pop();
+
+            if(step == k)
+                ans.push_back(node);
+
+            for(auto adjNode : hm[node])
+            {
+                if(visited.find(adjNode) == visited.end())
+                {
+                    q.push({adjNode, step+1});
+                    visited[adjNode] = 1;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
