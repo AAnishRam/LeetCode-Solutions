5d001d500bc9a8f946245eacd25401d6a195055a
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2025-02-20 11:48:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (91.35%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    void findCombinations(int sum, int i, vector<int>& candidates, int target, 
+
+    // see submission i where i used a seperate sum variable insted of doing it in target 
+
+    void findCombinations(int i, vector<int>& candidates, int target, 
                           vector<vector<int>>& ans, vector<int>& temp) {
-        if (sum == target) {
+        if (target == 0) {
             ans.push_back(temp);
             return;
         }
-        if (i == candidates.size() || sum > target) {
+        if (i == candidates.size() || target < 0) {
             return;
         }
 
         temp.push_back(candidates[i]);
-        findCombinations(sum + candidates[i], i, candidates, target, ans, temp);
+        findCombinations(i, candidates, target-candidates[i], ans, temp);
         temp.pop_back();
 
-        findCombinations(sum, i + 1, candidates, target, ans, temp);
+        findCombinations( i + 1, candidates, target, ans, temp);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> temp;
-        findCombinations(0, 0, candidates, target, ans, temp);
+        findCombinations( 0, candidates, target, ans, temp);
         return ans;
     }
 };
