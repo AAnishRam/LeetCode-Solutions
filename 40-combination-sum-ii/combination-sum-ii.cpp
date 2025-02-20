0e2d8ca9c5cc26ce4c4e1deb8901846b534a1794
ANISH RAM A <123292130+AAnishRam@users.noreply.github.com>
2025-02-20 12:43:47 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (27.19%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    void findCombination(int i, set<vector<int>> &ans, vector<int> &temp, 
+    vector<int>& candidates, int target)
+    {
+        if(target == 0)
+        {
+            ans.insert(temp);
+            return;
+        }
+
+        if(i == candidates.size() || target < 0)
+            return;
+
+        temp.push_back(candidates[i]);
+        findCombination(i+1, ans, temp, candidates, target - candidates[i]);
+        temp.pop_back();
+
+        while (i + 1 < candidates.size() && candidates[i] == candidates[i + 1])
+            i++;
+
+        findCombination(i+1, ans, temp, candidates, target);
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        set<vector<int>> ans;
+        vector<int>temp;
+        sort(candidates.begin(), candidates.end());
+
+        findCombination(0, ans, temp, candidates, target);
+
+        vector<vector<int>>fin;
+        for(auto it : ans)
+            fin.push_back(it);
+        return fin;
+    }
+};
